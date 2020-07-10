d41b8e4b2e355343190119c8d18de84e225f53cb
Snackhole <37544796+Snackhole@users.noreply.github.com>
2020-07-09 19:52:11 -0500

    Skeleton for interface creation

--- Interface/MainWindow.py
+++ Interface/MainWindow.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QFrame, QGridLayout, QMainWindow, QApplication, QMessageBox
 
 
 class MainWindow(QMainWindow):
@@ -20,10 +21,44 @@ class MainWindow(QMainWindow):
         self.Center()
 
     def CreateInterface(self):
-        pass
+        # Create Window Icon
+        # self.WindowIcon = QIcon(self.GetResourcePath("Assets/NomenEXIF Icon.png"))
+
+        # Window Icon and Title
+        # self.setWindowIcon(self.WindowIcon)
+        self.setWindowTitle(self.ScriptName)
+
+        # Create Central Frame
+        self.Frame = QFrame()
+
+        # Create Widgets
+
+        # Create Layout
+        self.Layout = QGridLayout()
+
+        # Widgets in Layout
+
+        # Set and Configure Layout
+        self.Frame.setLayout(self.Layout)
+
+        # Create Status Bar
+        self.StatusBar = self.statusBar()
+
+        # Set Central Frame
+        self.setCentralWidget(self.Frame)
 
     def GetResourcePath(self, RelativeLocation):
         return self.AbsoluteDirectoryPath + "/" + RelativeLocation
+
+    # Interface Methods
+    def DisplayMessageBox(self, Message, Icon=QMessageBox.Information, Buttons=QMessageBox.Ok, Parent=None):
+        MessageBox = QMessageBox(self if Parent is None else Parent)
+        MessageBox.setWindowIcon(self.WindowIcon)
+        MessageBox.setWindowTitle(self.ScriptName)
+        MessageBox.setIcon(Icon)
+        MessageBox.setText(Message)
+        MessageBox.setStandardButtons(Buttons)
+        return MessageBox.exec_()
 
     # Window Management Methods
     def Center(self):
