d5ad352cb83501686ddd1beb33d8e67428d06597
Snackhole <37544796+Snackhole@users.noreply.github.com>
2020-07-20 14:32:43 -0500

    Progress on renamer

--- Core/ExifRenamer.py
+++ Core/ExifRenamer.py
@@ -6,17 +6,21 @@ class ExifRenamer:
     def __init__(self):
         self.RenameQueue = []
         self.AvailableTags = set()
+        self.UniqueIdentifier = 1
 
     def AddToRenameQueue(self, Additions):
         for Addition in Additions:
             if Addition.endswith(".jpg") or Addition.endswith(".jpeg"):
+                AdditionAbsolutePath = os.path.abspath(Addition)
                 try:
-                    OpenedAddition = Image.open(Addition)
+                    OpenedAddition = Image.open(AdditionAbsolutePath)
                     AdditionExifData = {ExifTags.TAGS[NumericTag]: TagContents for NumericTag, TagContents in OpenedAddition._getexif().items() if NumericTag in ExifTags.TAGS}
                 except:
                     return False
-                AdditionData = {"Path": os.path.abspath(Addition), "ExifData": AdditionExifData}
-                self.RenameQueue.append(AdditionData)
+                AdditionData = {"Path": AdditionAbsolutePath, "Directory": os.path.dirname(AdditionAbsolutePath), "FileName": os.path.basename(AdditionAbsolutePath), "Extension": os.path.splitext(AdditionAbsolutePath)[1], "ExifData": AdditionExifData}
+                if AdditionData not in self.RenameQueue:
+                    self.RenameQueue.append(AdditionData)
+        self.RenameQueue.sort(key=lambda x: x["Path"])
         self.DetermineAvailableTags()
 
     def DetermineAvailableTags(self):
@@ -57,13 +61,26 @@ class ExifRenamer:
             FileName = FileName.replace("[HOUR]", Hour)
             FileName = FileName.replace("[MINUTE]", Minute)
             FileName = FileName.replace("[SECOND]", Second)
-        for Tag in self.AvailableTags:
+        for Tag in [AvailableTag for AvailableTag in self.AvailableTags if AvailableTag not in ["YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND"]]:
             FileName = FileName.replace("[" + Tag + "]", str(ExifData[Tag]))
         return FileName
 
     def RenameFilesWithTemplate(self, Template):
         for QueuedFile in self.RenameQueue:
             NewName = self.GenerateFileName(QueuedFile["ExifData"], Template)
+            PotentialNewPath = os.path.join(QueuedFile["Directory"], NewName + QueuedFile["Extension"])
+            if PotentialNewPath != QueuedFile["Path"]:
+                FilesInDirectory = os.listdir(QueuedFile["Directory"])
+                while NewName + QueuedFile["Extension"] in FilesInDirectory and PotentialNewPath != QueuedFile["Path"]:
+                    PreviousIdentifierString = " - " + str(self.UniqueIdentifier - 1)
+                    if NewName.endswith(PreviousIdentifierString):
+                        NewName = NewName[:-len(PreviousIdentifierString)]
+                    NewName += " - " + str(self.UniqueIdentifier)
+                    PotentialNewPath = os.path.join(QueuedFile["Directory"], NewName + QueuedFile["Extension"])
+                    self.UniqueIdentifier += 1
+                if PotentialNewPath != QueuedFile["Path"]:
+                    print("Renaming:  " + QueuedFile["FileName"] + " -> " + os.path.basename(PotentialNewPath))
+                    os.rename(QueuedFile["Path"], PotentialNewPath)
 
 
 # if __name__ == "__main__":
