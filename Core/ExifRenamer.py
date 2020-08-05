6541a0502d9fd7ea1a6498745a4750f1d00b5ccc
Snackhole <37544796+Snackhole@users.noreply.github.com>
2020-08-05 16:06:06 -0500

    Threading done

--- Core/ExifRenamer.py
+++ Core/ExifRenamer.py
@@ -1,4 +1,6 @@
 import os
+import queue
+import threading
 from PIL import Image, ExifTags
 
 
@@ -6,7 +8,6 @@ class ExifRenamer:
     def __init__(self):
         self.RenameQueue = []
         self.AvailableTags = set()
-        self.UniqueIdentifier = 1
 
     def AddToRenameQueue(self, Additions):
         Success = True
@@ -44,7 +45,7 @@ class ExifRenamer:
             self.AvailableTags.add("HOUR")
             self.AvailableTags.add("MINUTE")
             self.AvailableTags.add("SECOND")
-    
+
     def GetAvailableTags(self):
         AvailableTagsList = list(self.AvailableTags)
         AvailableTagsList.sort()
@@ -54,38 +55,65 @@ class ExifRenamer:
         self.RenameQueue.clear()
         self.AvailableTags.clear()
 
-    def GenerateFileName(self, ExifData, Template):
-        FileName = Template
-        if "DateTimeOriginal" in self.AvailableTags:
-            Year = str(ExifData["DateTimeOriginal"])[0:4]
-            Month = str(ExifData["DateTimeOriginal"])[5:7]
-            Day = str(ExifData["DateTimeOriginal"])[8:10]
-            Hour = str(ExifData["DateTimeOriginal"])[11:13]
-            Minute = str(ExifData["DateTimeOriginal"])[14:16]
-            Second = str(ExifData["DateTimeOriginal"])[17:19]
-            FileName = FileName.replace("[YEAR]", Year)
-            FileName = FileName.replace("[MONTH]", Month)
-            FileName = FileName.replace("[DAY]", Day)
-            FileName = FileName.replace("[HOUR]", Hour)
-            FileName = FileName.replace("[MINUTE]", Minute)
-            FileName = FileName.replace("[SECOND]", Second)
-        for Tag in [AvailableTag for AvailableTag in self.AvailableTags if AvailableTag not in ["YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND"]]:
-            FileName = FileName.replace("[" + Tag + "]", str(ExifData[Tag]))
-        return FileName
-
     def RenameFilesWithTemplate(self, Template):
-        for QueuedFile in self.RenameQueue:
-            NewName = self.GenerateFileName(QueuedFile["ExifData"], Template)
-            PotentialNewPath = os.path.join(QueuedFile["Directory"], NewName + QueuedFile["Extension"])
-            if PotentialNewPath != QueuedFile["Path"]:
-                FilesInDirectory = os.listdir(QueuedFile["Directory"])
-                while NewName + QueuedFile["Extension"] in FilesInDirectory and PotentialNewPath != QueuedFile["Path"]:
-                    PreviousIdentifierString = " - " + str(self.UniqueIdentifier - 1)
-                    if NewName.endswith(PreviousIdentifierString):
-                        NewName = NewName[:-len(PreviousIdentifierString)]
-                    NewName += " - " + str(self.UniqueIdentifier)
+        class RenameThread(threading.Thread):
+            def __init__(self, Template, FileQueue, AvailableTags):
+                # Store Parameters
+                self.Template = Template
+                self.FileQueue = FileQueue
+                self.AvailableTags = AvailableTags
+
+                # Variables
+                self.UniqueIdentifier = 1
+                self.RenameComplete = False
+                self.FilesRenamed = 0
+                self.FileQueueSize = self.FileQueue.qsize()
+
+                # Initialize
+                super().__init__(name="RenameThread", daemon=True)
+
+            def run(self):
+                while not self.FileQueue.empty():
+                    QueuedFile = self.FileQueue.get()
+                    NewName = self.GenerateFileName(QueuedFile["ExifData"], self.Template)
                     PotentialNewPath = os.path.join(QueuedFile["Directory"], NewName + QueuedFile["Extension"])
-                    self.UniqueIdentifier += 1
-                if PotentialNewPath != QueuedFile["Path"]:
-                    print("Renaming:  " + QueuedFile["FileName"] + " -> " + os.path.basename(PotentialNewPath))
-                    os.rename(QueuedFile["Path"], PotentialNewPath)
+                    if PotentialNewPath != QueuedFile["Path"]:
+                        FilesInDirectory = os.listdir(QueuedFile["Directory"])
+                        while NewName + QueuedFile["Extension"] in FilesInDirectory and PotentialNewPath != QueuedFile["Path"]:
+                            PreviousIdentifierString = " - " + str(self.UniqueIdentifier - 1)
+                            if NewName.endswith(PreviousIdentifierString):
+                                NewName = NewName[:-len(PreviousIdentifierString)]
+                            NewName += " - " + str(self.UniqueIdentifier)
+                            PotentialNewPath = os.path.join(QueuedFile["Directory"], NewName + QueuedFile["Extension"])
+                            self.UniqueIdentifier += 1
+                        if PotentialNewPath != QueuedFile["Path"]:
+                            print("Renaming:  " + QueuedFile["FileName"] + " -> " + os.path.basename(PotentialNewPath))
+                            os.rename(QueuedFile["Path"], PotentialNewPath)
+                    self.FilesRenamed += 1
+                self.RenameComplete = True
+
+            def GenerateFileName(self, ExifData, Template):
+                FileName = Template
+                if "DateTimeOriginal" in self.AvailableTags:
+                    Year = str(ExifData["DateTimeOriginal"])[0:4]
+                    Month = str(ExifData["DateTimeOriginal"])[5:7]
+                    Day = str(ExifData["DateTimeOriginal"])[8:10]
+                    Hour = str(ExifData["DateTimeOriginal"])[11:13]
+                    Minute = str(ExifData["DateTimeOriginal"])[14:16]
+                    Second = str(ExifData["DateTimeOriginal"])[17:19]
+                    FileName = FileName.replace("[YEAR]", Year)
+                    FileName = FileName.replace("[MONTH]", Month)
+                    FileName = FileName.replace("[DAY]", Day)
+                    FileName = FileName.replace("[HOUR]", Hour)
+                    FileName = FileName.replace("[MINUTE]", Minute)
+                    FileName = FileName.replace("[SECOND]", Second)
+                for Tag in [AvailableTag for AvailableTag in self.AvailableTags if AvailableTag not in ["YEAR", "MONTH", "DAY", "HOUR", "MINUTE", "SECOND"]]:
+                    FileName = FileName.replace("[" + Tag + "]", str(ExifData[Tag]))
+                return FileName
+
+        FileQueue = queue.Queue()
+        for QueuedFile in self.RenameQueue:
+            FileQueue.put(QueuedFile)
+
+        RenameThreadInst = RenameThread(Template, FileQueue, self.AvailableTags.copy())
+        RenameThreadInst.start()
