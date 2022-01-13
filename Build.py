93fde2c6bee40aa021b81758125faf8509732d27
Snackhole <37544796+Snackhole@users.noreply.github.com>
2022-01-13 13:26:22 -0600

    Bump to 8

--- Build.py
+++ Build.py
@@ -7,7 +7,7 @@ import zipapp
 
 # Build Variables
 BuildVariables = {}
-BuildVariables["Version"] = "7"
+BuildVariables["Version"] = "8"
 BuildVariables["AppName"] = "NomenExif"
 BuildVariables["VersionedAppName"] = BuildVariables["AppName"] + " " + BuildVariables["Version"]
 
